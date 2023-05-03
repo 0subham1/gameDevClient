7c80aff6db0afb75bda72b5d967d91eba93b7a58
Subham Gupta <0subhamgupta1@gmail.com>
2023-05-03 12:39:11 +0530

    a

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState(12345);
   const BASE_URL = "https://game-dev-server.vercel.app/";
+  // const BASE_URL = "http://localhost:4000/";
 
   const handleSave = () => {
     const data = {
@@ -15,7 +16,7 @@ const App = () => {
     axios.post(BASE_URL + "signup", data).then((res) => {
       console.log(res);
       if (res.data) {
-        alert("Order placed successfully");
+        alert("success");
       } else {
         alert("error ");
       }
